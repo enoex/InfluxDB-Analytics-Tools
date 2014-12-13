18266d1483430a843b9d1860808cba98a24f738d
Erik <enoex1@gmail.com>
2014-12-12 21:52:26 -0800

    updates examples

--- examples/server-mongo.js
+++ examples/server-mongo.js
@@ -22,6 +22,8 @@ var MongoClient = require('mongodb').MongoClient,
 var flatten = require('../lib/helpers').flatten;
 var analytics = require('../lib/analytics');
 
+var YOUR_COLLECTION = 'users';
+
 // Connect to Mongo and get stats
 // --------------------------------------
 // TODO: Chat messages should be on separate DB
@@ -41,36 +43,9 @@ MongoClient.connect(nconf.get('database').db, function(err, db) {
             function getBehaviorStats(callback){
                 var start = process.hrtime();
 
-                // Room Count
-                db.collection('rooms').aggregate([
-                    { $group: { _id: null, count: { $sum: 1 } } }
-                ], function ( err, result ){
-                    if(err || !result){ 
-                        logger.log('error:roomCount', 'error: ' + err);
-                        return; 
-                    }
-
-                    var diff = process.hrtime(start);
-                    var ms = (diff[0] * 1e9) + diff[1] / 1000000;
-
-                    if(result.length < 1){
-                        logger.log('warn:roomCount', 'nothing returned',
-                            ' | time: ' + ms + 'ms');
-                        analytics.track({
-                            group: 'app:rooms:count', queryTime: ms, 
-                            value: 0
-                        }); 
-                        return false;
-                    }
-
-                    result = +result[0].count;
-
-                    analytics.track({
-                        group: 'app:rooms:count', queryTime: ms, value: result
-                    }); 
-                });
-            
-                // User count
+                // ----------------------
+                // Example Query : User online count
+                // ----------------------
                 db.collection('users').aggregate([
                     { $match: { isActive: true } },
                     { $group: { _id: null, count: { $sum: 1 } } }
@@ -103,14 +78,16 @@ MongoClient.connect(nconf.get('database').db, function(err, db) {
                     }); 
                 });
 
-                // TODO: :::::: Move this to the different DB connection
-                // Chat Messages
                 // ----------------------
-                db.collection('chatmessages').aggregate([
+                // For number of docs in a specific collection
+                // ----------------------
+                // NOTE: Aggregation is used to ensure right counts for
+                // sharded configurations
+                db.collection(YOUR_COLLECTION).aggregate([
                     { $group: { _id: null, count: { $sum: 1 } } }
                 ], function ( err, result ){
                     if(err || !result ){ 
-                        logger.log('error:chatMessages', 'error: ' + err);
+                        logger.log('error:' + YOUR_COLLECTION, 'error: ' + err);
                         return; 
                     }
 
@@ -121,18 +98,16 @@ MongoClient.connect(nconf.get('database').db, function(err, db) {
                         logger.log('warn:roomCount', 'nothing returned',
                             ' | time: ' + ms + 'ms');
                         analytics.track({
-                            group: 'app:chatMessages:count', queryTime: ms, 
+                            group: 'app:' + YOUR_COLLECTION + ':count', queryTime: ms, 
                             value: 0
                         }); 
                         return false;
                     }
-
-
                     result = result[0].count;
 
                     // send to analytics
                     analytics.track({
-                        group: 'app:chatMessages:count', queryTime: ms,
+                        group: 'app:' + YOUR_COLLECTION + ':count', queryTime: ms,
                         value: result
                     }); 
                 });
@@ -143,9 +118,11 @@ MongoClient.connect(nconf.get('database').db, function(err, db) {
                 setTimeout(callback, 30);
             },
 
-            // --------------------------
+            // ==========================
+            //
             // aggregate / server stats
-            // --------------------------
+            //
+            // ==========================
             function getServerStats(callback){
                 // Server stats
                 // ----------------------
